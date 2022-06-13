02d65315b6a5069118edf4e6ef13aca129655ad3
romf-odoo <romf@odoo.com>
2022-06-13 18:24:32 +0000

    [ADD] Basic Module Odoo Space Mission

---
+++ Odoo_space_mission/__manifest__.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+{
+    'name': 'Odoo space mission',
+    'summary': 'Module to get to the moon',
+    'description': """Module to help organize the logistcs Including crew and the space ship""",
+    'author': 'Roberto',
+    'category': 'Trainning',
+    'version': '0.1',
+    'depends': ['base'],
+    'data': [],
+    'demo': [],
+    
+}
